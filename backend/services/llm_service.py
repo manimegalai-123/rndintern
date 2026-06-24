7e477c8818d143f4a96f21386176057696cc66cf
manimegalai-123 <sivakumarmanimegalai89@gmail.com>
2026-06-24 17:57:14 +0530

    Implement LLM service with Groq client

---
+++ backend/services/llm_service.py
@@ -0,0 +1,20 @@
+import os
+from groq import Groq
+
+client = Groq(
+    api_key=os.getenv("groq_key")
+)
+
+def ask_llm(prompt):
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
