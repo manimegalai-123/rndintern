e54ec13572156f0951d162e1284b3e6c3a74aac0
manimegalai-123 <sivakumarmanimegalai89@gmail.com>
2026-06-24 17:40:41 +0530

    Update API to use dynamic base URL from env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
-import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 
-const api = axios.create({
-    baseURL: "http://127.0.0.1:8000"
-});
+const response = await axios.post(
+  `${API_URL}/pipeline/`,
+  formData
+);
 
 
 
